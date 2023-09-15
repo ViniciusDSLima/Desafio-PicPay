61d304acc7619307aa0d5267c1ae15768814b37e
Vinicius <vinicius.devjvm@gmail.com>
2023-09-15 11:44:29 -0300

    Criado o controller e criado o metodo de salvar usuarios.

---
+++ src/main/java/com/example/desafiopicpay/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.desafiopicpay.controller;
+
+import com.example.desafiopicpay.request.UserRegisterRequest;
+import com.example.desafiopicpay.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/users")
+@AllArgsConstructor
+public class UserController {
+
+    private UserService userService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity save(@RequestBody UserRegisterRequest userRegisterRequest){
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userRegisterRequest));
+    }
+}
