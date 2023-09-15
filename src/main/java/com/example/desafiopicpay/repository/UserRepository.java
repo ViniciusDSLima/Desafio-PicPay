efbae44dde4d782382e1579de95cea295443986d
Vinicius <vinicius.devjvm@gmail.com>
2023-09-15 09:40:28 -0300

    refatorada a UserRepository para consertar a excecao lancada ao iniciar a aplicacao. (Long -> Id

--- src/main/java/com/example/desafiopicpay/repository/UserRepository.java
+++ src/main/java/com/example/desafiopicpay/repository/UserRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<Object> findById(String id);
+public interface UserRepository extends JpaRepository<User, String> {
 }
