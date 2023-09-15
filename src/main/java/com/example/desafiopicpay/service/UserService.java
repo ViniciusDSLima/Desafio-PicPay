554c2738c9e63b1f7c52b94bf55c28589616c3a5
Vinicius <vinicius.devjvm@gmail.com>
2023-09-15 09:10:53 -0300

    Criado duas validacoes: valida se o saldo e insuficiente; Valida de o valor da transferencia e igual a 0

--- src/main/java/com/example/desafiopicpay/service/UserService.java
+++ src/main/java/com/example/desafiopicpay/service/UserService.java
@@ -2,12 +2,18 @@ package com.example.desafiopicpay.service;
 
 import com.example.desafiopicpay.domain.User;
 import com.example.desafiopicpay.enums.TypeUser;
-import com.example.desafiopicpay.exceptions.ValidacaoLojistaTransacao;
+import com.example.desafiopicpay.exceptions.errors.InsufficientFunds;
+import com.example.desafiopicpay.exceptions.errors.ValidacaoLojistaTransacao;
+import com.example.desafiopicpay.exceptions.errors.ValueNotAllowed;
 import com.example.desafiopicpay.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+
+import static com.example.desafiopicpay.enums.errorsMessage.InsufficientFundsMessage.SALDO_INSUFICIENTE_MESSAGE;
+import static com.example.desafiopicpay.enums.errorsMessage.ValidacaoLogistaTransacaoMessage.LOGISTA_TRANSACAO_MESSAGE;
+import static com.example.desafiopicpay.enums.errorsMessage.ValueNotAllowedMessage.VALOR_NAO_PODE_SER_ZERO;
 
 @Service
 @AllArgsConstructor
@@ -16,8 +22,17 @@ public class UserService {
     private UserRepository userRepository;
 
     public void validationTransaction(User sender, BigDecimal amount){
+
         if(sender.getTypeUser() == TypeUser.LOJISTAS){
-            throw new ValidacaoLojistaTransacao("O lohista nao tem permisao para realizar a transacao ");
+            throw new ValidacaoLojistaTransacao(LOGISTA_TRANSACAO_MESSAGE.getDescription());
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new InsufficientFunds(SALDO_INSUFICIENTE_MESSAGE.getDescription());
+        }
+
+        if(amount.compareTo(BigDecimal.ZERO) == 0){
+            throw new ValueNotAllowed(VALOR_NAO_PODE_SER_ZERO.getDescription());
         }
     }
 }
